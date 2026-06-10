bd5a85d1f5f64bf688919cc38281098d6dc360c7
SothearithLay <hearith9099@gmail.com>
2026-06-10 11:38:53 +0700

    copy paste

---
+++ src/components/pages/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>About</h1>
+  <p>This is the about page.</p>
+  <button @click="goToDashboard">Go to Dashboard</button>
+  <button @click="goToSignIn">Go to Sign In</button>
+  <button @click="goToSignUp">Sign Up</button>
+</template>
+<script setup>
+import { useRouter } from "vue-router";
+const router = useRouter();
+function goToDashboard() {
+  router.push("/dashboard");
+}
+function goToSignIn() {
+  router.push({ name: "SignIn" });
+}
+function goToSignUp(){
+  router.push({ name:"SignUp"})
+}
+</script>
